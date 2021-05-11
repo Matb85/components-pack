40b01b0832e3a9ffe87f8d92b79915069504d2c9
Matb85 <69219238+Matb85@users.noreply.github.com>
2021-05-11 12:49:05 +0200

    fix(svelte-pack): :label: types

--- packages/svelte-pack/index.d.ts
+++ packages/svelte-pack/index.d.ts
@@ -1,9 +1,5 @@
 import { SvelteComponentTyped } from "svelte";
 import type { store } from "@matb85/base-pack";
-import { setup, Slidehandler, Noloop, lazyloading, buttons } from "modular-slider";
-
-const Slider = setup(Slidehandler, Noloop);
-export { lazyloading, buttons, Slider };
 
 type options = {
   target: HTMLElement;
@@ -12,6 +8,6 @@ type options = {
 export const Photo: new (options: options) => SvelteComponentTyped;
 export const PhotoViewer: new (options: options) => SvelteComponentTyped;
 export const PhotoMultiViewer: new (options: options) => SvelteComponentTyped;
-export const VuePackClass: new () => store.VuePackClass;
+export const Store: new () => store.Store;
 export const mutations: Record<string, (...args: any[]) => void>;
 export function init(): void;
