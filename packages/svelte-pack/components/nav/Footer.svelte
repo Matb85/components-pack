f8cacb960d04583badf98117ef1da74a6d98ca19
matb85 <mateuszbis85@gmail.com>
2021-08-28 09:12:03 +0200

    fix(svelte-pack): :bug: remove template tags

--- packages/svelte-pack/components/nav/Footer.svelte
+++ packages/svelte-pack/components/nav/Footer.svelte
@@ -1,28 +1,26 @@
-<template>
-  <footer class="footer {className}">
-    <div class="footer-bg">
-      <Photo prevent="{['enlargeonclick']}" src="{background}" sizes="{[720, 1290, 1920, 2400]}" className="no-hover" />
-      <div class="footer-text">
-        <slot />
+<footer class="footer {className}">
+  <div class="footer-bg">
+    <Photo prevent="{['enlargeonclick']}" src="{background}" sizes="{[720, 1290, 1920, 2400]}" className="no-hover" />
+    <div class="footer-text">
+      <slot />
+    </div>
+  </div>
+  <div class="footer-links">
+    {#each references as reference}
+      <div v-for="reference in references" :key="reference.id" class="footer-links-col">
+        <h3>{reference.name}</h3>
+        {#each reference.links as link}
+          <ul>
+            <li>
+              <a href="{link.url}">{link.name}</a>
+            </li>
+          </ul>
+        {/each}
       </div>
-    </div>
-    <div class="footer-links">
-      {#each references as reference}
-        <div v-for="reference in references" :key="reference.id" class="footer-links-col">
-          <h3>{reference.name}</h3>
-          {#each reference.links as link}
-            <ul>
-              <li>
-                <a href="{link.url}">{link.name}</a>
-              </li>
-            </ul>
-          {/each}
-        </div>
-      {/each}
-    </div>
-    <div class="footer-copyright">Copyright © {name} {new Date().getFullYear()}</div>
-  </footer>
-</template>
+    {/each}
+  </div>
+  <div class="footer-copyright">Copyright © {name} {new Date().getFullYear()}</div>
+</footer>
 
 <script>
 import Photo from "../Photo.svelte";
--- packages/svelte-pack/components/nav/Navbar.svelte
+++ packages/svelte-pack/components/nav/Navbar.svelte
@@ -1,20 +1,18 @@
-<template>
-  <nav class="navbar {className}">
-    <slot name="logo" />
-    <div class="nav" style="{isActive ? 'transform: translateX(-100%)' : ''}">
-      <slot />
+<nav class="navbar {className}">
+  <slot name="logo" />
+  <div class="nav" style="{isActive ? 'transform: translateX(-100%)' : ''}">
+    <slot />
+  </div>
+  <div
+    class="hamburger-container"
+    on:click="{() => {
+      isActive = !isActive;
+    }}">
+    <div class="hamburger {isActive ? 'is-active' : ''}">
+      <div class="hamburger-inner"></div>
     </div>
-    <div
-      class="hamburger-container"
-      on:click="{() => {
-        isActive = !isActive;
-      }}">
-      <div class="hamburger {isActive ? 'is-active' : ''}">
-        <div class="hamburger-inner"></div>
-      </div>
-    </div>
-  </nav>
-</template>
+  </div>
+</nav>
 
 <script>
 export let className;
