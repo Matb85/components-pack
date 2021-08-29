5aef56a9c436741ab10ce9cfbbddf7b244b7761b
matb85 <mateuszbis85@gmail.com>
2021-08-29 10:03:33 +0200

    fix(svelte-pack): :label: export types

--- packages/svelte-pack/index.d.ts
+++ packages/svelte-pack/index.d.ts
@@ -8,6 +8,8 @@ type options = {
 export const Photo: new (options: options) => SvelteComponentTyped;
 export const PhotoViewer: new (options: options) => SvelteComponentTyped;
 export const PhotoMultiViewer: new (options: options) => SvelteComponentTyped;
+export const Navbar: new (options: options) => SvelteComponentTyped;
+export const Footer: new (options: options) => SvelteComponentTyped;
 export const Store: () => StoreI;
 export const mutations: typeof Mutations;
 export function init(): void;
