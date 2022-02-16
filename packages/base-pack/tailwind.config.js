8758e39795049ec7d50efc8df7268b501f85ba80
matb85 <mateuszbis85@gmail.com>
2022-02-16 17:27:14 +0000

    feat(base-pack): :lipstick: make use of css variables

--- packages/base-pack/tailwind.config.js
+++ packages/base-pack/tailwind.config.js
@@ -2,11 +2,13 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: false,
-  darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
       white: colors.white,
       black: colors.black,
+      black: "var(--medium-pack-primary-color, #000)",
+      primary: "var(--medium-pack-primary-color, rgba(255,255,255,var(--tw-bg-opacity, 1)))",
+      secondary: "var(--medium-pack-secondary-color, rgba(0,0,0,var(--tw-bg-opacity, 1)))",
     },
   },
   variants: {
