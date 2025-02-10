42a243af41bfc8ce1f0816fb677c63b4e70c0c54
matb85 <mateuszbis85@gmail.com>
2025-02-10 12:15:28 +0100

    feat: create astro-pack

---
+++ packages/astro-pack/astro.config.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import { astroPack } from "./components/integration.js";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		astroPack({
+			formats: {
+				thumbnail: /min_|thumbnail_/,
+				2400: '',
+				1920: 'fhd_',
+				1290: 'hd_',
+				720: 'wvga_',
+				480: 'hvga_'
+			},
+			enlarged: [1290, 1920]
+		}),
+		starlight({
+			title: '@Matb85/components-pack',
+			social: {
+				github: 'https://github.com/Matb85/components-pack',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
