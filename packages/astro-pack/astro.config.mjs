d36e7d0d76eadd9e4c00118c3cc15301da5319a4
matb85 <mateuszbis85@gmail.com>
2025-02-10 18:14:24 +0100

    ci: deploy docs

--- packages/astro-pack/astro.config.mjs
+++ packages/astro-pack/astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import { astroPack } from "./dist/integration.js";
+import { astroPack } from "./out/integration.js";
 
 // https://astro.build/config
 export default defineConfig({
