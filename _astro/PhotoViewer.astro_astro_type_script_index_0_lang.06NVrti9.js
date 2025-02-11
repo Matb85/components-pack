57f991b253eb914d552a75b27a809057eeb52949
Matb85 <Matb85@users.noreply.github.com>
2025-02-11 21:35:09 +0000

    deploy: 2959ff748c160b6ac6757bbdfc37440978ec0a42

---
+++ _astro/PhotoViewer.astro_astro_type_script_index_0_lang.06NVrti9.js
@@ -0,0 +1,1 @@
+import{b as n}from"./index.Dy4Ri3mE.js";import{G as o}from"./astro-pack.C0yKslky.js";const e=document.getElementById("MP-viewer-viewed-photo");window.addEventListener("enlargePhoto",t=>n.mounted(t.detail,e.parentElement,e,o));window.addEventListener("keyup",t=>{t.key==="Escape"&&n.close(e.parentElement,e)});e.parentElement.addEventListener("click",()=>n.close(e.parentElement,e));
