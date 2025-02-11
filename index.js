1a350c7f6969b6c1e25d153134cbdd4ecf1c06a3
Matb85 <Matb85@users.noreply.github.com>
2025-02-11 12:03:18 +0000

    deploy: 0317f1c6a97fff518d94b52ff34032f02d0ec1d9

--- index.js
+++
@@ -1,3 +0,0 @@
-export function addPhotos(photos) {
-    window.dispatchEvent(new CustomEvent("MP-addPhotos", { detail: photos }));
-}
--- integration.d.ts
+++
@@ -1,3 +0,0 @@
-import type { GlobalConfigI } from "@matb85/base-pack";
-import type { AstroIntegration } from "astro";
-export declare function astroPack(module: GlobalConfigI): AstroIntegration;
