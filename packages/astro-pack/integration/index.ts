6889245705e18dc5bf8b5f2abee5407c30c9abf6
matb85 <mateuszbis85@gmail.com>
2025-07-11 16:17:00 +0200

    fix(astro-pack): :label: TS types again

--- packages/astro-pack/integration/index.ts
+++ packages/astro-pack/integration/index.ts
@@ -1,15 +1,3 @@
-import type { GlobalConfigI, BasePackStoreI } from "@matb85/base-pack";
-
-declare module "virtual:@matb85/astro-pack" {
-  export const GlobalConfig: GlobalConfigI;
-}
-
-declare global {
-  interface Window {
-    cpStore: BasePackStoreI;
-  }
-}
-
 export function addPhotos(photos: NodeListOf<HTMLImageElement>) {
   window.dispatchEvent(new CustomEvent<NodeListOf<HTMLImageElement>>("MP-addPhotos", { detail: photos }));
 }
